7f8d24e861117c557895e54451b98435cf508c3b
AnnelotJ <janssenannelot@gmail.com>
2026-07-03 23:04:59 +0200

    hello

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string.h>
+
+struct word{
+    
+}
 
 int main(){
 
-    std::cout<<"hello world";
+    std::string inputString;
+    
+    // RETRIEVE & STORE
+    std::cout<<"The following instructions are possible:\n STORE \n RETRIEVE\n";
+    std::getline(std::cin,inputString);
+    
+    
 
     return 0;
 }
