aecedbdb55a9c59073b7204f5488370f4f777150
annelotjanssen <janssenannelot@gmail.com>
2026-07-05 19:52:59 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <string.h>
 
-struct word{
-    
-}
+struct Word{
+    std::string characters; 
+};
+
+
 
 int main(){
 
     std::string inputString;
     
     // RETRIEVE & STORE
-    std::cout<<"The following instructions are possible:\n STORE \n RETRIEVE\n";
+    std::cout<<"The following instructions are possible:\nSTORE \nRETRIEVE\n";
     std::getline(std::cin,inputString);
-    
-    
+
+    std::cout<< inputString;
+
+    if (inputString.find("STORE")){
+        std::cout<<"STORE\n";
+
+    }
+    else if (inputString.find("RETRIEVE")){
+        std::cout<<"RETRIEVE";
+
+    }
+    else{
+        std::cout<<"Option not possible terminating program"; 
+        exit(0);
+    }
+
 
     return 0;
 }
